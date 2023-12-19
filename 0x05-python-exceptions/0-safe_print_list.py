9507842a61f09d7de094e4ca62de037089f96024
yabsera <yabfeker633@gmail.com>
2023-12-19 00:12:51 -0800

    safe_print_list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    m = 0 
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            m += 1
+        except IndexError:
+            break
+    print()
+    return m
