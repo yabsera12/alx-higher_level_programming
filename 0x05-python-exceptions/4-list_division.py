d203a418977062506463e7e9b13c92430117d794
yabsera <yabfeker633@gmail.com>
2023-12-19 01:53:38 -0800

    4-list_division.py

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-# 4-list_division.py
-# Stephen Oba <obastepheno@gmail.com>
-
 def list_division(my_list_1, my_list_2, list_length):
-    """Divide the integers in two lists"""
     new_list = []
     result = 0
     for i in range(list_length):
