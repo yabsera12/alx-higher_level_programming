9e66b6edd9fbbf1d7fc746ce8b3bbb89e111ec2b
yabsera <yabfeker633@gmail.com>
2023-12-21 17:52:41 -0800

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
-# 0-square.py by UDO INNOCENT
-"""class of square """
+"""Defines a square """
 
 class Square:
-    """Represents a square"""
-
-    def __init__(self, size):
-        """Initializing this square class
-        Args: size - represnets the size of the square
-        """
-
-        self.__size = size
+    """Does nothing"""
+    pass
